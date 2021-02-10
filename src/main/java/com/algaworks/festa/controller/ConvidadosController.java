2250a08326bd6030171791c431d5d8a9707b310e
Raul Castro <raulc27@gmail.com>
2021-02-10 03:18:06 -0300

    1st push...

---
+++ src/main/java/com/algaworks/festa/controller/ConvidadosController.java
@@ -0,0 +1,39 @@
+package com.algaworks.festa.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+import  com.algaworks.festa.repository.Convidados;
+import com.algaworks.festa.model.Convidado;
+
+@Controller
+public class ConvidadosController {
+	
+	@Autowired
+	private Convidados convidados;
+	
+	
+	@GetMapping("/convidados")
+		public ModelAndView listar() {
+		
+		
+		ModelAndView modelAndView = new ModelAndView("ListaConvidados");
+		modelAndView.addObject("convidados",convidados.findAll());
+		modelAndView.addObject("convidado", new Convidado());
+		
+		return modelAndView;
+		
+		
+	}
+	
+	@PostMapping("/convidados")
+	public String salvar(Convidado convidado) {
+		this.convidados.save(convidado);
+		return "redirect:/convidados";
+	}
+	
+}
